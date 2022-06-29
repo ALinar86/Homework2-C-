674fc8f37e6f13d4b1da47aeafc02f5377f79b67
Linar <alr86@mail.ru>
2022-06-29 23:28:21 +0300

    add Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿/*void FindMiddleNumber(int a)
+{
+    int b = a / 10;
+    int c = b % 10;
+    Console.Write("Second number is: " + c);
+}
+
+Console.Write("Input three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+FindMiddleNumber(number);*/
+
+int FindMiddleNumber(int a)
+{
+    int b = a / 10;
+    int c = b % 10;
+    return c;
+}
+
+int result;
+Console.Write("Input three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+result = FindMiddleNumber(number);
+Console.WriteLine("Second number is: " + result);
