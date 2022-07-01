8cae1146bd7dc0ad7bcd87afbbe1d49176d8d531
Linar <alr86@mail.ru>
2022-07-01 19:59:58 +0300

    add Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿void Dayoff(int dayNumder)
+{
+    if (dayNumder == 6 || dayNumder == 7)
+    {
+        Console.WriteLine("Today is a day off.");
+    }
+    else if (dayNumder >= 1 && dayNumder <= 5)
+    {
+        Console.WriteLine("No, today is weekdays.");
+    }
+    else
+    {
+        Console.WriteLine("Incorrect number, try again.");
+    }
+}
+
+Console.Write("Input day numder: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Dayoff(number);
