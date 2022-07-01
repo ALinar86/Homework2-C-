652322fa45148ba9b6687ac423c13efd2fa880dd
Linar <alr86@mail.ru>
2022-07-01 19:25:09 +0300

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿void FindThirdDigit(int x)
+{
+    if (x > 99) 
+    {
+        int digit = ((x / 100) % 10);
+        Console.WriteLine("Третья цифра: " + digit);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет.");
+    }
+}
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+FindThirdDigit(number);
